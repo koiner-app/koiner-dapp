cf8f0b625a0cd967a9f9c04577000de223974160
Frank Weijers <frank@weijers.io>
2022-04-06 23:25:32 +0200

    Add Kondor

---
+++ src/boot/kondor.ts
@@ -0,0 +1,28 @@
+import { boot } from 'quasar/wrappers';
+import { Provider, Signer } from 'koilib';
+
+interface KondorInstance {
+  provider: Provider;
+  signer: Signer;
+  getAccounts: any;
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $kondor: KondorInstance;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const kondor = window.kondor;
+console.log('kondor?');
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+console.log(kondor);
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$kondor = kondor;
+});
+
+export { kondor };
---
+++ src/components/UserMenu.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-if="!account.connected">
+    <span @click="account.login()">Connect wallet</span>
+  </div>
+  <q-list
+    v-if="account.connected"
+    bordered
+    padding
+    class="rounded-borders text-primary bottom-nav cursor-pointer"
+  >
+    <q-item v-if="account" flat class="full-width">
+      <q-menu anchor="bottom right" self="bottom left">
+        <div class="row no-wrap q-pa-md">
+          <div class="column items-center">
+            <div class="text-subtitle1 q-mt-md q-mb-lg">
+              Address: {{ account.address }}
+            </div>
+
+            <q-btn
+              v-close-popup
+              color="primary"
+              label="Logout"
+              push
+              size="sm"
+              @click="account.logout"
+            />
+          </div>
+        </div>
+      </q-menu>
+      <q-item-section avatar>
+        <q-avatar size="2rem" color="primary" text-color="white">
+          <span style="font-size: 0.75rem">ME</span>
+        </q-avatar>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useAccountStore } from 'stores/account';
+
+export default defineComponent({
+  setup() {
+    const account = useAccountStore();
+
+    return {
+      account,
+    };
+  },
+});
+</script>
