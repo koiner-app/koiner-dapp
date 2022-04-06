e8925da622be5d40a57b0d0942f1a50f5d3f69dd
Frank Weijers <frank@weijers.io>
2022-04-06 23:26:24 +0200

    Setup account store

---
+++ src/stores/account.ts
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineStore, acceptHMRUpdate } from 'pinia';
+import { kondor } from 'boot/kondor';
+
+export const useAccountStore = defineStore({
+  id: 'account',
+  state: () => ({
+    addresses: [] as string[],
+    connected: false,
+  }),
+
+  getters: {
+    address: (state) => state.addresses[0],
+  },
+
+  actions: {
+    logout() {
+      this.$patch({
+        addresses: [],
+        connected: false,
+      });
+    },
+
+    /**
+     * Attempt to login a user using Kondor wallet
+     */
+    async login() {
+      const accounts = await kondor.getAccounts();
+
+      if (accounts.length > 0 && accounts[0].address) {
+        this.$patch({
+          addresses: [accounts[0].address],
+          connected: true,
+        });
+      }
+    },
+  },
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useAccountStore, import.meta.hot));
+}
