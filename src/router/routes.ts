a9e8289d9745616b133294d09de4b91bf9373550
Frank Weijers <frank@weijers.io>
2022-05-27 22:59:06 +0200

    Add health check page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,10 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/health',
+    component: () => import('pages/HealthCheckPage.vue'),
+  },
+  {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
