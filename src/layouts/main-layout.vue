5b135e933a5ca6f14f9504f3f4a1f6b6aaa3d89d
Frank Weijers <frank@weijers.io>
2022-10-15 00:12:30 +0200

    Remove global actions user menu for now

--- src/layouts/main-layout.vue
+++ src/layouts/main-layout.vue
@@ -6,26 +6,11 @@
           <koiner-logo />
         </q-toolbar-title>
 
+        <search-dialog />
+
         <q-btn flat dense size="md" color="accent"
           ><q-icon name="favorite_border"
         /></q-btn>
-
-        <search-dialog />
-
-        <global-actions-menu />
-
-        <q-btn
-          v-if="account.connected"
-          flat
-          dense
-          size="md"
-          :to="`/addresses/${account.address}`"
-          ><q-icon name="account_balance_wallet"
-        /></q-btn>
-
-        <q-separator />
-
-        <user-menu />
       </q-toolbar>
     </q-header>
 
@@ -52,31 +37,24 @@ import { defineComponent, ref } from 'vue';
 import KoinerLogo from 'components/koiner-logo.vue';
 import MainNavigation from 'components/main-navigation.vue';
 import Ticker from 'components/ticker-component.vue';
-import UserMenu from 'components/user-menu.vue';
-import GlobalActionsMenu from 'components/global-actions-menu.vue';
 import SearchDialog from '@koiner/components/search/search-dialog.vue';
-import { useAccountStore } from 'stores/account';
 import { useKoinosStore } from 'stores/koinos';
 
 export default defineComponent({
   name: 'MainLayout',
   components: {
     SearchDialog,
-    GlobalActionsMenu,
-    UserMenu,
     MainNavigation,
     Ticker,
     KoinerLogo,
   },
   setup() {
     const koinos = useKoinosStore();
-    const account = useAccountStore();
     const leftDrawerOpen = ref(false);
 
     koinos.load();
 
     return {
-      account,
       leftDrawerOpen,
     };
   },
