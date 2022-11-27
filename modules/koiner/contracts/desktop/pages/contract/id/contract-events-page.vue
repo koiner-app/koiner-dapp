ba24846d5fee07c13f45ca3ff3f9eb73161380f8
Frank Weijers <frank@weijers.io>
2022-11-27 21:23:39 +0100

    Load token events + operations when contract is token type

--- modules/koiner/contracts/desktop/pages/contract/id/contract-events-page.vue
+++ modules/koiner/contracts/desktop/pages/contract/id/contract-events-page.vue
@@ -1,32 +1,73 @@
 <template>
-  <q-page class="row items-baseline justify-evenly">
+  <q-page class="row items-baseline justify-evenly" v-if="contract">
     <q-card class="table-card shadow-1">
       <q-card-section>
-        <contract-events-table title="Events" :contract-ids="[id]" />
+        <tokens-events-table
+          v-if="contract.contractStandardType === 'token'"
+          title="Events"
+          :contract-ids="[id]"
+        />
+        <contract-events-table v-else title="Events" :contract-ids="[id]" />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, Ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { defineComponent, onMounted, ref, Ref, watch } from 'vue';
 import ContractEventsTable from '../../../../components/contract/search/view/contracts-events-table.vue';
+import TokensEventsTable from '@koiner/tokenize/components/event/search/view/tokens-events-table.vue';
+import { useRoute } from 'vue-router';
+import { ItemState } from '@appvise/search-manager';
+import { Contract, useContractQuery } from '@koiner/sdk';
 
 export default defineComponent({
   name: 'ContractEventsPage',
-  components: { ContractEventsTable },
+  components: { TokensEventsTable, ContractEventsTable },
 
   setup() {
     let id: Ref<string | undefined> = ref();
+
+    const itemState = ItemState.create<Contract>();
+    const variables: Ref<{ id: string }> = ref({ id: '' });
     const route = useRoute();
+
+    const executeQuery = () => {
+      const { data, fetching, error, isPaused } = useContractQuery({
+        variables,
+      });
+
+      watch(data, (updatedData) => {
+        itemState.item.value = updatedData?.contract as Contract;
+      });
+
+      itemState.error = error;
+      itemState.fetching = fetching;
+      itemState.isPaused = isPaused;
+    };
 
     onMounted(async () => {
       id.value = route.params.id.toString();
+      variables.value.id = route.params.id.toString();
+      executeQuery();
+      itemState.isPaused.value = true;
+      itemState.isPaused.value = false;
     });
+
+    watch(
+      () => route.params.id,
+      async (newId) => {
+        itemState.isPaused.value = !newId;
+        variables.value.id = newId ? newId.toString() : '';
+      }
+    );
 
     return {
       id,
+
+      itemState,
+      contract: itemState.item,
+      error: itemState.error,
     };
   },
 });
--- modules/koiner/contracts/desktop/pages/contract/id/contract-operations-page.vue
+++ modules/koiner/contracts/desktop/pages/contract/id/contract-operations-page.vue
@@ -1,32 +1,77 @@
 <template>
-  <q-page class="row items-baseline justify-evenly">
+  <q-page class="row items-baseline justify-evenly" v-if="contract">
     <q-card class="table-card shadow-1">
       <q-card-section>
-        <contract-operations-table title="Operations" :contract-ids="[id]" />
+        <tokens-operations-table
+          v-if="contract.contractStandardType === 'token'"
+          title="Operations"
+          :contract-ids="[id]"
+        />
+        <contract-operations-table
+          v-else
+          title="Operations"
+          :contract-ids="[id]"
+        />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, Ref } from 'vue';
+import { defineComponent, onMounted, ref, Ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import ContractOperationsTable from '../../../../components/contract/search/view/contracts-operations-table.vue';
+import { Contract, useContractQuery } from '@koiner/sdk';
+import { ItemState } from '@appvise/search-manager';
+import TokensOperationsTable from '@koiner/tokenize/components/operation/search/view/tokens-operations-table.vue';
 
 export default defineComponent({
   name: 'ContractOperationsPage',
-  components: {ContractOperationsTable },
+  components: { TokensOperationsTable, ContractOperationsTable },
 
   setup() {
     let id: Ref<string | undefined> = ref();
+
+    const itemState = ItemState.create<Contract>();
+    const variables: Ref<{ id: string }> = ref({ id: '' });
     const route = useRoute();
+
+    const executeQuery = () => {
+      const { data, fetching, error, isPaused } = useContractQuery({
+        variables,
+      });
+
+      watch(data, (updatedData) => {
+        itemState.item.value = updatedData?.contract as Contract;
+      });
+
+      itemState.error = error;
+      itemState.fetching = fetching;
+      itemState.isPaused = isPaused;
+    };
 
     onMounted(async () => {
       id.value = route.params.id.toString();
+      variables.value.id = route.params.id.toString();
+      executeQuery();
+      itemState.isPaused.value = true;
+      itemState.isPaused.value = false;
     });
+
+    watch(
+      () => route.params.id,
+      async (newId) => {
+        itemState.isPaused.value = !newId;
+        variables.value.id = newId ? newId.toString() : '';
+      }
+    );
 
     return {
       id,
+
+      itemState,
+      contract: itemState.item,
+      error: itemState.error,
     };
   },
 });
