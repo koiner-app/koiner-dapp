a3d9a7308f77a29c637178c7f110b2bc6e1ae9c4
Frank Weijers <frank@weijers.io>
2022-11-30 00:27:32 +0100

    Use DataAttributeRenderer for contract events + operations

--- modules/koiner/renderers.ts
+++ modules/koiner/renderers.ts
@@ -1,9 +1,11 @@
+import { KoinerAttributeRenderers } from '@koiner/components/search/attribute';
 import { KoinerChainRenderers } from '@koiner/chain/components/renderers';
 import { KoinerContractRenderers } from '@koiner/contracts/components/renderers';
 import { KoinerNetworkRenderers } from '@koiner/network/components/renderers';
 import { KoinerTokenizeRenderers } from '@koiner/tokenize/components/renderers';
 
 export const KoinerRenderers = [
+  ...KoinerAttributeRenderers,
   ...KoinerChainRenderers,
   ...KoinerContractRenderers,
   ...KoinerNetworkRenderers,
