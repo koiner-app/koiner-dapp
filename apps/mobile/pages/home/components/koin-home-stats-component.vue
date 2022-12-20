07fac23e0cba6e2627724925707a081320a4403a
Frank Weijers <frank@weijers.io>
2022-12-21 00:08:41 +0100

    Setup mobile home page with in development warning banner

---
+++ apps/mobile/pages/home/components/koin-home-stats-component.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="large-stats-cards">
+    <counter-metric
+      class="token-price"
+      title="KOIN Price"
+      :value="statsStore.formattedKoinPrice"
+      :footer="{
+        title: 'Marketcap:',
+        value: statsStore.formattedMarketCap,
+      }"
+      :footer-tooltips="[
+        {
+          title: 'Virtual Marketcap',
+          value: statsStore.formattedVirtualMarketCap,
+          unit: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;KOIN + VHP',
+        },
+        {
+          title: 'Fully Diluted Valuation',
+          value: statsStore.formattedFDVMarketCap,
+          unit: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All tokens claimed',
+        },
+        {
+          title: 'FDV Virtual MC',
+          value: statsStore.formattedVirtualFDVMarketCap,
+          unit: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All tokens claimed + VHP',
+        },
+      ]"
+      :tooltip-title-width="125"
+      :tooltip-item-width="250"
+    />
+
+    <counter-metric
+      title="Burned Koin"
+      :value="statsStore.totalSupply.burned"
+      unit="%"
+      :decimals="2"
+      :footer="{
+        title: 'Virtual Supply:',
+        value: statsStore.formattedVirtualTotalSupply(),
+        unit: koinerStore.koinContract.symbol,
+      }"
+      :footer-tooltips="[
+        {
+          title: 'KOIN Total Supply',
+          value: statsStore.formattedKoinTotalSupply(),
+          unit: koinerStore.koinContract.symbol,
+        },
+        {
+          title: 'VHP Total Supply',
+          value: statsStore.formattedVhpTotalSupply(),
+          unit: koinerStore.vhpContract.symbol,
+        },
+      ]"
+    />
+
+    <counter-metric
+      title="Transactions"
+      :value="statsStore.chainStats.transactionCount"
+      :footer="{
+        title: 'Token transfer events:',
+        value: statsStore.tokenStats.transferCount,
+      }"
+    />
+
+    <counter-metric
+      title="Claimed Koin"
+      :value="statsStore.totalSupply.claimed"
+      unit="%"
+      :decimals="2"
+      :footer="{
+        title: 'Total accounts:',
+        value: statsStore.chainStats.addressCount,
+      }"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useKoinosStore } from 'stores/koinos';
+import { useKoinerStore } from 'stores/koiner';
+import { useStatsStore } from 'stores/stats';
+import CounterMetric from '@koiner/components/metrics/counter-metric.vue';
+import { useAccountStore } from 'stores/account';
+
+export default defineComponent({
+  name: 'KoinosHomeStatsComponent',
+  components: { CounterMetric },
+  setup() {
+    const koinosStore = useKoinosStore();
+    const koinerStore = useKoinerStore();
+    const statsStore = useStatsStore();
+    const accountStore = useAccountStore();
+
+    return {
+      koinosStore,
+      koinerStore,
+      statsStore,
+      accountStore,
+    };
+  },
+});
+</script>
--- apps/mobile/pages/home/index-page.vue
+++ apps/mobile/pages/home/index-page.vue
@@ -1,13 +1,41 @@
 <template>
   <q-page class="row items-start">
-    Home
+    <q-banner
+      class="bg-primary text-white"
+      v-if="!accountStore.dismissedMobileBanner"
+    >
+      Mobile version is being developed. Please use a larger screen to view more
+      data.
+      <template v-slot:action>
+        <q-btn
+          flat
+          color="white"
+          label="Dismiss"
+          @click="accountStore.dismissedMobileBanner = true"
+        />
+      </template>
+    </q-banner>
+    <koinos-home-stats-component />
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, Ref, ref, watch } from 'vue';
+import KoinosHomeStatsComponent from './components/koin-home-stats-component.vue';
+import { useAccountStore } from 'stores/account';
 
 export default defineComponent({
-  name: 'MobileHomeIndexPage',
+  name: 'MobileHomePage',
+  components: {
+    KoinosHomeStatsComponent,
+  },
+
+  setup() {
+    const accountStore = useAccountStore();
+
+    return {
+      accountStore,
+    };
+  },
 });
 </script>
