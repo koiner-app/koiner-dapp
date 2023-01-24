f0fbbf251d8206262cef846c246d9e66db3249aa
Frank Weijers <frank@weijers.io>
2023-01-25 00:01:25 +0100

    Update koiner logo in projects

--- modules/koiner/ecosystem/projects/dapp/koiner.ts
+++ modules/koiner/ecosystem/projects/dapp/koiner.ts
@@ -4,7 +4,7 @@ export const koiner: Project = {
   name: 'Koiner',
   types: ['dApp'],
   caption: 'Dashboard',
-  image: '/projects/dapp/koiner.jpg',
+  image: '/projects/dapp/koiner.png',
   description:
     'Koiner Dashboard make the blockchain data of the Koinos ecosystem accessible.',
   links: [{ url: 'https://koiner.app', icon: 'fa-solid fa-globe' }],
