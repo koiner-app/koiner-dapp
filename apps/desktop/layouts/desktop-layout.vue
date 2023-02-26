3066971b59809dccc59333001d4594d693aedaa7
Frank Weijers <frank@weijers.io>
2023-02-26 11:32:53 +0100

    Set page container min height

--- apps/desktop/layouts/desktop-layout.vue
+++ apps/desktop/layouts/desktop-layout.vue
@@ -17,7 +17,7 @@
       </q-scroll-area>
     </q-drawer>
 
-    <q-page-container :class="`page--${route.name}`" style="height: 100vh">
+    <q-page-container :class="`page--${route.name}`" style="min-height: 100vh">
       <router-view />
     </q-page-container>
 
