3242d3dfbd683c8cd0a3622c32b9136b27e725db
Frank Weijers <frank@weijers.io>
2023-03-01 22:47:55 +0100

    Add drawer to mobile clean layout

--- apps/mobile/layouts/mobile-clean-layout.vue
+++ apps/mobile/layouts/mobile-clean-layout.vue
@@ -1,4 +1,10 @@
 <template>
+  <q-drawer :width="200" v-model="drawer" bordered>
+    <q-scroll-area class="fit">
+      <mobile-drawer-navigation />
+    </q-scroll-area>
+  </q-drawer>
+
   <q-page-container :class="`page--${route.name.replace('.', '-')}`">
     <router-view />
   </q-page-container>
@@ -7,9 +13,11 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import MobileDrawerNavigation from '@mobile/components/mobile-drawer-navigation.vue';
 
 export default defineComponent({
   name: 'MobileCleanLayout',
+  components: { MobileDrawerNavigation },
 
   setup() {
     const route = useRoute();
