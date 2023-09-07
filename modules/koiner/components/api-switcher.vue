62b0c7f24545144b578c15e69d816f674775160b
Frank Weijers <frank@weijers.io>
2023-09-08 00:02:49 +0200

    Disable api switcher

--- modules/koiner/components/api-switcher.vue
+++ modules/koiner/components/api-switcher.vue
@@ -1,18 +1,15 @@
 <template>
-  <q-btn
-    @click="koinerStore.switch"
-    v-if="koinerStore.isProd"
-    class="q-px-sm q-mx-sm"
-    size="sm"
-  >
+  <!--  @click="koinerStore.switch"-->
+  <q-btn v-if="koinerStore.isProd" class="q-px-sm q-mx-sm" size="sm">
     <q-avatar size="16px">
       <img alt="Koinos Mainnet" src="~/assets/koinos-icon-dark.svg" />
     </q-avatar>
     <span class="q-ml-xs" style="font-size: 0.625rem; font-weight: normal"
       >Mainnet</span
     >
-    <q-tooltip>Switch to testnet</q-tooltip>
+    <!--    <q-tooltip>Switch to testnet</q-tooltip>-->
   </q-btn>
+
   <q-btn
     @click="koinerStore.switch"
     v-if="!koinerStore.isProd"
