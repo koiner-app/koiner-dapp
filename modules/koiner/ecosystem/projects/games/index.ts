ff707737d24bb5050b3f27ce6551440ec89af37c
Frank Weijers <frank@weijers.io>
2023-10-24 23:34:33 +0200

    Add Space Striker to projects

--- modules/koiner/ecosystem/projects/games/index.ts
+++ modules/koiner/ecosystem/projects/games/index.ts
@@ -1,4 +1,5 @@
 import { kanvas } from './kanvas';
 import { koinosRaffles } from './koinos-raffles';
+import { spaceStriker } from './space-striker';
 
-export const games = [kanvas, koinosRaffles];
+export const games = [kanvas, koinosRaffles, spaceStriker];
---
+++ modules/koiner/ecosystem/projects/games/space-striker.ts
@@ -0,0 +1,18 @@
+import { Project } from '@koiner/ecosystem';
+
+export const spaceStriker: Project = {
+  name: 'Space Striker',
+  types: ['Game', 'dApp'],
+  caption: 'Game',
+  image: '/projects/game/space-striker.png',
+  description:
+    'A fun game where you need to defend Planet Koinos from the aliens. Play the game for free and win a reward!',
+  links: [
+    {
+      url: 'https://kanvas-app.com/space_striker/',
+      icon: 'fa-solid fa-globe',
+    },
+  ],
+  maintainers: [],
+  tags: ['Game'],
+};
