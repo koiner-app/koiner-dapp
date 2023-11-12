88c5660b94efad8d605feed5eb4f9ea29d495e9d
Frank Weijers <frank@weijers.io>
2023-11-12 01:56:14 +0100

    Add CryptoBulls to projects

--- modules/koiner/ecosystem/projects/games/index.ts
+++ modules/koiner/ecosystem/projects/games/index.ts
@@ -1,5 +1,6 @@
 import { kanvas } from './kanvas';
 import { koinosRaffles } from './koinos-raffles';
 import { spaceStriker } from './space-striker';
+import { cryptoBulls } from './crypto-bulls';
 
-export const games = [kanvas, koinosRaffles, spaceStriker];
+export const games = [kanvas, koinosRaffles, spaceStriker, cryptoBulls];
