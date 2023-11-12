4cca084f161e10d8db64415272b5861b05adfbae
Frank Weijers <frank@weijers.io>
2023-11-12 01:45:37 +0100

    Add KoinCrew token

--- modules/koiner/ecosystem/projects/content/koincrew.ts
+++ modules/koiner/ecosystem/projects/content/koincrew.ts
@@ -6,6 +6,10 @@ export const koinCrew: Project = {
   caption: 'Content',
   icon: 'fa-solid fa-book',
   description: 'Articles and guides about Koinos and the Koinos ecosystem.',
+  token: {
+    name: 'KCT',
+    contractId: '1BTQCpospHJRA7VAtZ4wvitdcqYCvkwBCD',
+  },
   links: [{ url: 'https://koincrew.com/', icon: 'fa-solid fa-globe' }],
   maintainers: [],
   tags: ['Articles', 'Guides'],
