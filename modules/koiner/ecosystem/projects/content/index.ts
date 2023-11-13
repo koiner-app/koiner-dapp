cd47ababd2f8531a9a9eaee3a859f553abe97314
Frank Weijers <frank@weijers.io>
2023-11-13 23:18:03 +0100

    Add Koincast to projects

--- modules/koiner/ecosystem/projects/content/index.ts
+++ modules/koiner/ecosystem/projects/content/index.ts
@@ -1,11 +1,13 @@
 import { koinPress } from './koinpress';
 import { youtubeMotoengineer } from './youtube-motoengineer';
-import { youtubeKoinos } from '@koiner/ecosystem/projects/content/youtube-koinos';
-import { koinCrew } from '@koiner/ecosystem/projects/content/koincrew';
+import { youtubeKoinos } from './youtube-koinos';
+import { koinCrew } from './koincrew';
+import { koincast } from './koincast';
 
 export const contentProjects = [
   youtubeKoinos,
   koinPress,
   youtubeMotoengineer,
   koinCrew,
+  koincast,
 ];
---
+++ modules/koiner/ecosystem/projects/content/koincast.ts
@@ -0,0 +1,19 @@
+import { Project } from '@koiner/ecosystem';
+
+export const koincast: Project = {
+  name: 'Koincast',
+  types: ['Content'],
+  caption: 'Podcast',
+  image: '/projects/content/koincast.jpg',
+  description:
+    'A podcast on blockchains, decentralized economy, dApps, web 3.0, liberal thinking, and artificial intelligence',
+  links: [
+    { url: 'https://koincast.io/', icon: 'fa-solid fa-globe' },
+    {
+      url: 'https://x.com/koincast',
+      icon: 'fa-brands fa-twitter',
+    },
+  ],
+  maintainers: [],
+  tags: ['Podcast'],
+};
