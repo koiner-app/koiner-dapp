40645d5b93712d3fc6530cee40642aa74180a7cd
Frank Weijers <frank@weijers.io>
2024-01-25 23:00:56 +0100

    Add header to mobile home page

--- apps/mobile/pages/home/index-page.vue
+++ apps/mobile/pages/home/index-page.vue
@@ -1,4 +1,13 @@
 <template>
+  <q-header reveal elevated>
+    <q-toolbar>
+      <q-separator dark vertical inset class="lt-md" />
+      <q-toolbar-title>
+        <koiner-logo />
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+
   <q-page class="row items-start q-pt-xs q-pb-xl">
     <koinos-home-stats-component />
   </q-page>
@@ -8,10 +17,12 @@
 import { defineComponent } from 'vue';
 import KoinosHomeStatsComponent from './components/koin-home-stats-component.vue';
 import { useAccountStore } from 'stores/account';
+import KoinerLogo from 'components/koiner-logo.vue';
 
 export default defineComponent({
   name: 'MobileHomePage',
   components: {
+    KoinerLogo,
     KoinosHomeStatsComponent,
   },
 
