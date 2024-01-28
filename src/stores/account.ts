b4db0cfde8a16f4a8f6d6d259dd241bb9bec63cb
Frank Weijers <frank@weijers.io>
2024-01-28 21:17:26 +0100

    Remove log

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -452,8 +452,6 @@ export const useAccountStore = defineStore({
 
       watch(data, async (updatedData) => {
         if (this[this.environment].loading) {
-          console.log('ALREAD LOAYING!');
-
           return;
         }
 
