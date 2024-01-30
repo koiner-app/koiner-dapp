8463103bce37915cc41e8fd964412fd0fbfc63d5
Frank Weijers <frank@weijers.io>
2024-01-30 22:11:03 +0100

    Remove onchain token balances workarounds

--- modules/koiner/tokenize/components/holder/search/view/token-holders-table.vue
+++ modules/koiner/tokenize/components/holder/search/view/token-holders-table.vue
@@ -12,7 +12,6 @@
     :uischema="uiSchema"
     :request="request"
     :data="{}"
-    @change="onChange"
     :additional-renderers="renderers"
   />
 </template>
@@ -25,8 +24,6 @@ import QJsonSearch from '@appvise/q-json-forms/QJsonSearch.vue';
 import schema from '../token-holders-search.schema.json';
 import mobileUiSchema from './token-holders-table.mobile-ui-schema.json';
 import desktopUiSchema from './token-holders-table.ui-schema.json';
-import { TokenHoldersConnection } from '@koiner/sdk';
-import { useTokensStore } from 'stores/tokens';
 
 export default defineComponent({
   name: 'TokenHoldersTable',
@@ -48,9 +45,6 @@ export default defineComponent({
   },
 
   setup(props) {
-    const tokensStore = useTokensStore();
-    const handled: Ref<string[]> = ref([]);
-
     return {
       schema,
       uiSchema: props.mobile ? mobileUiSchema : desktopUiSchema,
@@ -58,40 +52,6 @@ export default defineComponent({
         filter: {
           contractId: { equals: props.contractId },
         },
-      },
-      onChange: async (data: TokenHoldersConnection) => {
-        if (data.edges) {
-          const onChainBalances: Record<string, number> = {};
-
-          data.edges?.forEach((edge) => {
-            if (!handled.value.includes(edge.node.addressId)) {
-              onChainBalances[edge.node.addressId] = 0;
-
-              // Prevent double handling
-              handled.value.push(edge.node.addressId);
-            }
-          });
-
-          // Fetch on chain balances for all onChainBalances at once
-          const balances = await tokensStore.balances(
-            props.contractId,
-            Object.keys(onChainBalances)
-          );
-
-          for (const [address, balance] of Object.entries(balances)) {
-            onChainBalances[address] = balance;
-          }
-
-          data.edges?.map(async (edge) => {
-            // Map on chain balances to results
-            if (onChainBalances[edge.node.addressId] != null) {
-              edge.node.balance =
-                onChainBalances[edge.node.addressId].toString();
-            }
-
-            return edge;
-          });
-        }
       },
       renderers: KoinerRenderers,
     };
