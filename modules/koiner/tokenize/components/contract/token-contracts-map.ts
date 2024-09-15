de28c285d5f5eebd89cc47aaa4ebbc26437ddd1f
Frank Weijers <frank@weijers.io>
2024-09-15 23:59:10 +0200

    Rename VHP in tokenContractsMap

--- modules/koiner/tokenize/components/contract/token-contracts-map.ts
+++ modules/koiner/tokenize/components/contract/token-contracts-map.ts
@@ -1,5 +1,5 @@
 export const tokenContractsMap: Record<string, string> = {
-  '1AdzuXSpC6K9qtXdCBgD5NUpDNwHjMgrc9': 'VHP [deprecated]',
+  '1AdzuXSpC6K9qtXdCBgD5NUpDNwHjMgrc9': 'Virtual Hash Power [deprecated]',
   '15zQzktjXHPRstPYB9dqs6jUuCUCVvMGB9': 'Bitcoin [Chainge]',
   '15twURbNdh6S7GVXhqVs6MoZAhCfDSdoyd': 'Ethereum [Chainge]',
   '14MjxccMUZrtBPXnNkuAC5MLtPev2Zsk3N': 'Tether USD [Chainge]',
