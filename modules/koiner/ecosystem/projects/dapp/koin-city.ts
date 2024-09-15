735a466e9680bbd1d0f9c912c3623068d81c951c
Frank Weijers <frank@weijers.io>
2024-09-15 23:58:13 +0200

    Rename Koin City to Koincity

--- modules/koiner/ecosystem/projects/dapp/koin-city.ts
+++ modules/koiner/ecosystem/projects/dapp/koin-city.ts
@@ -1,12 +1,12 @@
 import { Project } from '@koiner/ecosystem';
 
 export const koinCity: Project = {
-  name: 'Koin City',
+  name: 'Koincity',
   types: ['dApp'],
   caption: 'Decentralized Exchange',
   image: '/projects/dapp/koincity.png',
   description:
-    'KoinCity is a crypto platform with a launchpad, decentralized exchange and social chat.',
+    'Koincity is a crypto platform with a launchpad, decentralized exchange and social chat.',
   links: [
     {
       url: 'https://koincity.com',
