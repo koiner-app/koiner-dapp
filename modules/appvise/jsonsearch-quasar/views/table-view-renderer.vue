66aea84d004a1a45ae6a15371ef085a4d32f651f
Frank Weijers <frank@weijers.io>
2024-09-15 20:26:36 +0200

    Disable skeleton loader table view for now

--- modules/appvise/jsonsearch-quasar/views/table-view-renderer.vue
+++ modules/appvise/jsonsearch-quasar/views/table-view-renderer.vue
@@ -47,55 +47,55 @@
       </q-td>
     </template>
 
-    <template v-slot:loading>
-      <q-markup-table class="no-shadow" v-if="!initialized">
-        <thead>
-          <tr>
-            <th class="text-left" style="width: 150px">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-            <th class="text-right">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-            <th class="text-right">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-            <th class="text-right">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-            <th class="text-right">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-            <th class="text-right">
-              <q-skeleton animation="blink" type="text" />
-            </th>
-          </tr>
-        </thead>
+    <!--    <template v-slot:loading>-->
+    <!--      <q-markup-table class="no-shadow" v-if="!initialized">-->
+    <!--        <thead>-->
+    <!--          <tr>-->
+    <!--            <th class="text-left" style="width: 150px">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--            <th class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--            <th class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--            <th class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--            <th class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--            <th class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" />-->
+    <!--            </th>-->
+    <!--          </tr>-->
+    <!--        </thead>-->
 
-        <tbody>
-          <tr v-for="n in 10" :key="n">
-            <td class="text-left">
-              <q-skeleton animation="blink" type="text" width="85px" />
-            </td>
-            <td class="text-right">
-              <q-skeleton animation="blink" type="text" width="50px" />
-            </td>
-            <td class="text-right">
-              <q-skeleton animation="blink" type="text" width="35px" />
-            </td>
-            <td class="text-right">
-              <q-skeleton animation="blink" type="text" width="65px" />
-            </td>
-            <td class="text-right">
-              <q-skeleton animation="blink" type="text" width="25px" />
-            </td>
-            <td class="text-right">
-              <q-skeleton animation="blink" type="text" width="85px" />
-            </td>
-          </tr>
-        </tbody>
-      </q-markup-table>
-    </template>
+    <!--        <tbody>-->
+    <!--          <tr v-for="n in 20" :key="n">-->
+    <!--            <td class="text-left">-->
+    <!--              <q-skeleton animation="blink" type="text" width="85px" />-->
+    <!--            </td>-->
+    <!--            <td class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" width="50px" />-->
+    <!--            </td>-->
+    <!--            <td class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" width="35px" />-->
+    <!--            </td>-->
+    <!--            <td class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" width="65px" />-->
+    <!--            </td>-->
+    <!--            <td class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" width="25px" />-->
+    <!--            </td>-->
+    <!--            <td class="text-right">-->
+    <!--              <q-skeleton animation="blink" type="text" width="85px" />-->
+    <!--            </td>-->
+    <!--          </tr>-->
+    <!--        </tbody>-->
+    <!--      </q-markup-table>-->
+    <!--    </template>-->
 
     <template v-slot:no-data>
       <div
